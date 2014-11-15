683bc0808a461eb8990b678207cb5fa9e7a5bebe
Klaas Ardinois <klaas.ardinois@gmail.com>
2014-11-15 13:59:58 +0000

    cleanup and setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+git clone git@github.com:bling/vim-airline ~/.vim/bundle/vim-airline
+git clone git@github.com:kien/ctrlp.vim ~/.vim/bundle/ctrlp
+git clone git@github.com:ervandew/supertab ~/.vim/bundle/supertab
+git clone git@github.com:scrooloose/syntastic ~/.vim/bundle/syntastic
